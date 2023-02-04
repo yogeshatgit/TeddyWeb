f1375878bd34eae26e274161a5785cbee287f000
Yogesh Gaikwad <119051813+yogeshatgit@users.noreply.github.com>
2023-02-04 21:40:53 +0530

    Update MyController.java
    updated @value attribute

--- src/main/java/com/teddyweb/controller/MyController.java
+++ src/main/java/com/teddyweb/controller/MyController.java
@@ -1,5 +1,6 @@
 package com.teddyweb.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,35 +13,22 @@ import java.io.File;
 @CrossOrigin(origins = "http://localhost:4200")
 public class MyController {
 
-    @GetMapping("")
+    @Value("${upload.folder}")
+    private String path;
 
+    @GetMapping("")
     public String homePage(){
         return "Home page";
     }
     @PostMapping("/{category}")
     public void uploadFile(@RequestParam("file") MultipartFile file, @PathVariable("category") String category){
-
         System.out.println("category= "+category);
-
+        System.out.println();
         String fileName = file.getOriginalFilename();
         try {
-            file.transferTo( new File("/home/yogesh/Desktop/Work/TeddyWeb1/src/assets/files/"+ fileName));
+            file.transferTo( new File(path+ fileName));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
-
-
-   /* @PostMapping("/table/{tableName}/file-data")
-    public ResponseEntity<String> loadFileData(@RequestParam("file") MultipartFile file, @PathVariable("tableName") String tableName){
-        try{
-            dataManagerService.loadFileData(tableName,file);
-            return new ResponseEntity<>("{\"message\": \"File data load successfully.\"}", HttpStatus.OK);
-        }
-        catch (Exception e){
-            String correlationId = UUID.randomUUID().toString();
-            LOGGER.error(correlationId+" File data load failed. "+tableName,e);
-            return new ResponseEntity<>(correlationId+" File data load failed. "+tableName,HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }*/
 }
